2bfbce40d6bffe28b20f4f9154f5ee6bc4734735
Vinay Bhat <vinaybhathg11@gmail.com>
2023-11-28 23:03:47 +0530

    feat: slice for toolbox

--- drawingtool/src/components/Toolbox/index.js
+++ drawingtool/src/components/Toolbox/index.js
@@ -1,15 +1,25 @@
 import { COLORS, MENU_ITEMS } from "@/constants";
 import styles from "./index.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeColor, changeBrushSize } from "@/slice/toolboxSlice";
+import cx from "classnames";
 
 const Toolbox = () => {
+  const dispatch = useDispatch();
   const activeMenuItem = useSelector((state) => state.menu.activeMenuItem);
-
   const showStrokeToolOption = activeMenuItem === MENU_ITEMS.PENCIL;
   const showBrushToolOption =
     activeMenuItem === MENU_ITEMS.ERASER || MENU_ITEMS.PENCIL;
+  const { color } = useSelector((state) => state.toolbox[activeMenuItem]);
 
-  const updateBrushSize = (e) => {};
+  const updateBrushSize = (e) => {
+    dispatch(changeBrushSize({ item: activeMenuItem, size: e.target.value }));
+  };
+
+  const updateColor = (newColor) => {
+    dispatch(changeColor({ item: activeMenuItem, color: newColor }));
+  };
+
   return (
     <div className={styles.toolboxContainer}>
       {showStrokeToolOption && (
@@ -17,28 +27,46 @@ const Toolbox = () => {
           <h4 className={styles.toolText}>Stroke Color</h4>
           <div className={styles.itemContainer}>
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLACK,
+              })}
               style={{ backgroundColor: COLORS.BLACK }}
+              onClick={() => updateColor(COLORS.BLACK)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.RED,
+              })}
               style={{ backgroundColor: COLORS.RED }}
+              onClick={() => updateColor(COLORS.RED)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.GREEN,
+              })}
               style={{ backgroundColor: COLORS.GREEN }}
+              onClick={() => updateColor(COLORS.GREEN)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.BLUE,
+              })}
               style={{ backgroundColor: COLORS.BLUE }}
+              onClick={() => updateColor(COLORS.BLUE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.ORANGE,
+              })}
               style={{ backgroundColor: COLORS.ORANGE }}
+              onClick={() => updateColor(COLORS.ORANGE)}
             />
             <div
-              className={styles.colorBox}
+              className={cx(styles.colorBox, {
+                [styles.active]: color === COLORS.YELLOW,
+              })}
               style={{ backgroundColor: COLORS.YELLOW }}
+              onClick={() => updateColor(COLORS.YELLOW)}
             />
           </div>
         </div>
--- drawingtool/src/store.js
+++ drawingtool/src/store.js
@@ -1,7 +1,9 @@
 const { configureStore } = require("@reduxjs/toolkit");
 import MenuReducer from "@/slice/menuSlice";
+import ToolboxReducer from "@/slice/toolboxSlice";
 export const store = configureStore({
   reducer: {
     menu: MenuReducer,
+    toolbox: ToolboxReducer,
   },
 });
